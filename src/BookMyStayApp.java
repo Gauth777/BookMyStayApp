48eb36eb39a4248271bd3d2f0656e2ac1c0c0722
Gauth777 <allenbarr2609@gmail.com>
2026-03-17 14:20:10 +0530

    UC7: Add-On Service Selection

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class AddOnService {
     private String serviceName;
     private double cost;
@@ -18,7 +17,7 @@ class AddOnService {
     }
 }
 
-class AddOnServiceManager {
+ class AddOnServiceManager {
     private Map<String, List<AddOnService>> servicesByReservation;
 
     public AddOnServiceManager() {
@@ -26,18 +25,19 @@ class AddOnServiceManager {
     }
 
     public void addService(String reservationId, AddOnService service) {
-        servicesByReservation
-                .computeIfAbsent(reservationId, k -> new ArrayList<>())
-                .add(service);
+        if (!servicesByReservation.containsKey(reservationId)) {
+            servicesByReservation.put(reservationId, new ArrayList<>());
+        }
+        servicesByReservation.get(reservationId).add(service);
     }
 
     public double calculateTotalServiceCost(String reservationId) {
+        double total = 0.0;
         List<AddOnService> services = servicesByReservation.get(reservationId);
-        if (services == null) return 0.0;
-
-        double total = 0;
-        for (AddOnService s : services) {
-            total += s.getCost();
+        if (services != null) {
+            for (AddOnService service : services) {
+                total += service.getCost();
+            }
         }
         return total;
     }
@@ -49,18 +49,18 @@ public class BookMyStayApp {
 
         String reservationId = "Single-1";
 
-        AddOnService breakfast = new AddOnService("Breakfast", 500);
-        AddOnService spa = new AddOnService("Spa", 700);
-        AddOnService pickup = new AddOnService("Airport Pickup", 300);
+        AddOnService s1 = new AddOnService("Breakfast", 500);
+        AddOnService s2 = new AddOnService("Spa", 700);
+        AddOnService s3 = new AddOnService("Airport Pickup", 300);
 
-        manager.addService(reservationId, breakfast);
-        manager.addService(reservationId, spa);
-        manager.addService(reservationId, pickup);
+        manager.addService(reservationId, s1);
+        manager.addService(reservationId, s2);
+        manager.addService(reservationId, s3);
 
-        double totalCost = manager.calculateTotalServiceCost(reservationId);
+        double total = manager.calculateTotalServiceCost(reservationId);
 
         System.out.println("Add-On Service Selection");
         System.out.println("Reservation ID: " + reservationId);
-        System.out.println("Total Add-On Cost: " + totalCost);
+        System.out.println("Total Add-On Cost: " + total);
     }
 }
